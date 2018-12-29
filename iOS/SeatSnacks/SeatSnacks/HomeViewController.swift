3d6c63e4b6a72ac8fb4ec168839150af77cbfe78
Michael Williams <michael.williams9824@gmail.com>
2018-12-29 01:58:46 -0500

    Added logout functionality

--- iOS/SeatSnacks/SeatSnacks/HomeViewController.swift
+++ iOS/SeatSnacks/SeatSnacks/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class HomeViewController: UIViewController {
 
@@ -16,7 +17,18 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func LogOut(_ sender: Any) {
+        do {
+            try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let initial = storyboard.instantiateInitialViewController()
+        UIApplication.shared.keyWindow?.rootViewController = initial
+    }
+    
     /*
     // MARK: - Navigation
 
--- iOS/SeatSnacks/SeatSnacks/StartViewController.swift
+++ iOS/SeatSnacks/SeatSnacks/StartViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //if Auth.auth().currentUser != nil {
+        //    self.performSegue(withIdentifier: "alreadyLoggedIn", sender: nil)
+        //}
     }
 
 
